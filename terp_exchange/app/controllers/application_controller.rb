fea201de6da804c36cc1e24d46ab5649334d9c35
John P. Dickerson <dickerson@cs.cmu.edu>
2016-11-03 18:43:15 -0400

    Splash screen
    Added in Bootstrap along with a basic view for the index.

--- terp_exchange/app/controllers/application_controller.rb
+++ terp_exchange/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def splash
+    @controller_text = "Text from the controller!"
+    render :index   # <-- name of the view, index.html.erb
+  end
+
 end
