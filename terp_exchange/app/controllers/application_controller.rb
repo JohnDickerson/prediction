8f217204176455fce5de1fb821f4a4c35d4e9f8b
Joseph Bergman <bergmancoding@gmail.com>
2016-11-09 23:52:14 -0500

    Added FB authentication

--- terp_exchange/app/controllers/application_controller.rb
+++ terp_exchange/app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
     render :index   # <-- name of the view, index.html.erb
   end
 
+  # Access the currently logged in user
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 end
--- terp_exchange/config/routes.rb
+++ terp_exchange/config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # root 'controller_name#action_name'
   root 'application#splash'
 
+  
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+
 end
