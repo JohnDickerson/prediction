bec3fa6232c4da997201f7c961e256cddf96c053
Ayman <akarim1@terpmail.umd.edu>
2016-12-08 16:25:56 -0500

    Ajax Calls

--- terp_exchange/config/routes.rb
+++ terp_exchange/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
-  match 'market/:mid', to: 'application#marketData', via: [:get]
+  match 'graph/:mid.json', to: 'application#marketData', via: [:get]
   match 'graph/:mid', to: 'application#marketGraph', via: [:get]
 
 end
