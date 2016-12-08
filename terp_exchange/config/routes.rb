44de56a0adc6587f12fdafecac12235832317198
Ayman <akarim1@terpmail.umd.edu>
2016-12-08 11:52:22 -0500

    Dynamic Market JSON

--- terp_exchange/config/routes.rb
+++ terp_exchange/config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
-  match 'market/1', to: 'application#marketData', via: [:get]
+  match 'market/:id', to: 'application#marketData', via: [:get]
 
 end
